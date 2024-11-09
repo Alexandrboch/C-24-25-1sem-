50c1518637f7260117573aec8472c316b994d86c
Alexandrboch <al_boch21@mail.ru>
2024-11-09 04:52:41 +0300

    lab_2 commit 1

--- lab_2.cpp
+++ lab_2.cpp
@@ -18,6 +18,39 @@ private:
     Grid<T>& operator=(Grid<T>&) = delete;
     Grid<T>& operator=(Grid<T>&&) = delete;
 
+public:
+    /*
+    Grid<T>(const Grid<T>& g)
+    {
+       data = new T[g.y_size*g.x_size];
+       y_size = g.y_size;
+       x_size = g.x_size;
+       for()
+    }
+    //заготовка копирующего конструктора
+    */
+
+    Grid<T>(T const &t): y_size(1), x_size(1), data(new T[1]) //1.1
+    {
+        *data = t;
+    }
+
+    Grid<T>(size_type y_size, size_type x_size): y_size(y_size), x_size(x_size), data(new T[y_size*x_size]) //1.2
+    {
+        for(size_type i=0; i < y_size*x_size; ++i)
+        {
+            *(data+i) = T();
+        }
+    }
+
+    Grid<T>(size_type y_size, size_type x_size, T const &t): y_size(y_size), x_size(x_size), data(new T[y_size*x_size]) //1.3
+    {
+        for(size_type i=0; i < y_size*x_size; ++i)
+        {
+            *(data+i) = t;
+        }
+    }
+
     T operator()(size_type y_idx, size_type x_idx) const
     {
         return data[y_idx * x_size + x_idx];
@@ -37,13 +70,37 @@ private:
         return *this;
     }
 
-    size_type get_y_size() const { return y_size; }
-    size_type get_x_size() const { return x_size; }
+    T * operator[](size_type y_idx) const //2
+    {
+        if(y_idx>=0 && y_idx<y_size)
+            return data + x_size*y_idx;
+        else
+            std::cout<<"Error: Out of range"<<std::endl;
+            return NULL;
+    }
+
+    size_type get_y_size() const
+    {
+        return y_size;
+    }
+
+    size_type get_x_size() const
+    {
+         return x_size;
+    }
+
+    ~Grid<T>()
+    {
+        delete [] data;
+    }
 };
 
 int main()
 {
-    /*Grid<float> g(3, 2, 0.0f);
+    Grid<float> g(3, 2, 0.0f);
+
+    std::cout<<g[1][1]<<std::endl;
+
     assert(3 == g.get_y_size());
     assert(2 == g.get_x_size());
 
@@ -53,14 +110,20 @@ int main()
         for(gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
             assert(0.0f == g[y_idx][x_idx]);
 
+    std::cout<<g[1][1]<<std::endl;
+
     for(gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
         for(gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
             g[y_idx][x_idx] = 1.0f;
 
+    std::cout<<g[1][1]<<std::endl;
+
     for(gsize_t y_idx = 0; y_idx != g.get_y_size(); ++y_idx)
         for(gsize_t x_idx = 0; x_idx != g.get_x_size(); ++x_idx)
             assert(1.0f == g(y_idx, x_idx));
-    */
+
+    std::cout<<g[1][1]<<std::endl;
+
 
     return 0;
 }
