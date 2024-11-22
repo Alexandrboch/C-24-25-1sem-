eb1d85ad8d45267046709ea08c373365455a85e9
Alexandrboch <al_boch21@mail.ru>
2024-11-23 02:32:59 +0300

    Fix lab 2.0

--- lab_2/lab_2.cpp
+++ lab_2/lab_2.cpp
@@ -8,37 +8,34 @@ public:
     using value_type = T;
     using size_type = unsigned;
 private:
-    T * const data;
-    size_type const y_size, x_size;
+    T *data;
+    size_type y_size, x_size;
 
     Grid(T *data, size_type y_size, size_type x_size): data(data), y_size(y_size), x_size(x_size) {}
 
-    Grid(Grid<T> const &) = delete;
-    Grid(Grid<T>&&) = delete;
-    Grid<T>& operator=(Grid<T>&) = delete;
-    Grid<T>& operator=(Grid<T>&&) = delete;
+    //Grid(Grid<T>&&) = delete;
+    //Grid<T>& operator=(Grid<T>&&) = delete;
 
 public:
 
-    /*
-    Grid<T>(Grid<T> const &scr) //заготовка копирующего конструктора (чтобы работал нужно убрать const)
+    Grid<T>(Grid<T> const &scr) // задание 2 копирующий конструктор
     {
-        data = new T[scr.y_size*scr.x_size];
         y_size = scr.y_size;
         x_size = scr.x_size;
+
+        data = reinterpret_cast<T*>(operator new(sizeof(T)*scr.y_size*scr.x_size));
         for(size_type y = 0; y < y_size; y++)
             for(size_type x = 0; x < x_size; x++)
-                *(data + y*x_size +x) = scr[y][x];
-
+                new (data + y*x_size + x) T(scr[y][x]);
     }
-    */
 
-    /*Grid<T>& operator= (Grid<T> const &scr) //заготовка оператора копирующего присваивания (чтобы работал нужно убрать const)
+
+    Grid<T>& operator= (Grid<T> const &scr) //задание 2 оператор копирующего присваивания
     {
-        T * new_data = new T[scr.y_size*scr.x_size];
+        T *new_data = reinterpret_cast<T*>(operator new(sizeof(T)*scr.y_size*scr.x_size));
         for(size_type y = 0; y < scr.y_size; y++)
             for(size_type x = 0; x < scr.x_size; x++)
-                *(new_data + y*scr.x_size +x) = scr[y][x];
+                new (new_data + y*x_size + x) T(scr[y][x]);
         delete [] data;
         data = new_data;
         y_size = scr.y_size;
@@ -46,17 +43,16 @@ public:
         return *this;
 
     }
-    */
 
 
-    Grid<T>(T const &t): y_size(1), x_size(1), data(reinterpret_cast<T*>(operator new(sizeof(T)))) //1.1
+    Grid<T>(T const &t): y_size(1), x_size(1), data(reinterpret_cast<T*>(operator new(sizeof(T)))) // задание 1.1
     {
         new (data) T(t);
     }
 
 
     /*
-    Grid<T>(size_type y_size, size_type x_size): y_size(y_size), x_size(x_size), data(new T[y_size*x_size)) //1.2
+    Grid<T>(size_type y_size, size_type x_size): y_size(y_size), x_size(x_size), data(new T[y_size*x_size)) // задание 1.2 (другой вариант)
     {
         for(size_type i=0; i < y_size*x_size; ++i)
         {
@@ -65,7 +61,7 @@ public:
     }
     */
 
-    Grid<T>(size_type y_size, size_type x_size): y_size(y_size), x_size(x_size), data(reinterpret_cast<T*>(operator new(sizeof(T)*y_size*x_size))) //1.2
+    Grid<T>(size_type y_size, size_type x_size): y_size(y_size), x_size(x_size), data(reinterpret_cast<T*>(operator new(sizeof(T)*y_size*x_size))) // задание 1.2
     {
         for(size_type i=0; i < y_size*x_size; ++i)
         {
@@ -73,7 +69,7 @@ public:
         }
     }
 
-    Grid<T>(size_type y_size, size_type x_size, T const &t): y_size(y_size), x_size(x_size), data(reinterpret_cast<T*>(operator new(sizeof(T)*y_size*x_size))) //1.3
+    Grid<T>(size_type y_size, size_type x_size, T const &t): y_size(y_size), x_size(x_size), data(reinterpret_cast<T*>(operator new(sizeof(T)*y_size*x_size))) // задание 1.3
     {
         for(size_type i=0; i < y_size*x_size; ++i)
         {
@@ -100,7 +96,7 @@ public:
         return *this;
     }
 
-    T * operator[](size_type y_idx) const //2
+    T * operator[](size_type y_idx) const // задание 2
     {
         if(y_idx>=0 && y_idx<y_size)
             return data + x_size*y_idx;
@@ -133,7 +129,13 @@ int main()
 {
     Grid<float> g(3, 2, 0.0f);
 
-    std::cout<<g[1][1]<<std::endl;
+    Grid<float> m(g);
+
+    Grid<float> k(2, 2, 0.12324f);
+
+    m=k;
+
+    std::cout<<m[1][1]<<std::endl;
 
     assert(3 == g.get_y_size());
     assert(2 == g.get_x_size());
